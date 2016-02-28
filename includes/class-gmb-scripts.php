e9c2657bc873b69a4e14220c481a829fde05689b
Josh Pollock <jpollock412@gmail.com>
2016-02-28 15:33:56 -0500

    Use new Google_Maps_Builder_Core_Scripts_Init class from core  https://github.com/WordImpress/maps-builder-core/issues/33

--- includes/class-gmb-scripts.php
+++ includes/class-gmb-scripts.php
@@ -14,33 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-class Google_Maps_Builder_Scripts  {
-
-	/**
-	 * Plugin slug
-	 *
-	 * @since 2.0.0
-	 *
-	 * @var string
-	 */
-	protected $plugin_slug;
-
-	/**
-	 * Load scripts by context
-	 *
-	 * @since 2.1.0
-	 */
-	public function __construct(){
-		$this->plugin_slug = Google_Maps_Builder::instance()->get_plugin_slug();
-		if( is_admin() ){
-			new Google_Maps_Builder_Core_Admin_Scripts();
-			add_action( 'admin_enqueue_scripts', array( $this, 'admin_hooks' ) );
-		}else{
-			 new Google_Maps_Builder_Core_Front_End_Scripts();
-
-		}
-
-	}
+class Google_Maps_Builder_Scripts extends Google_Maps_Builder_Core_Scripts_Init {
 
 	/**
 	 * Load additional admin scripts
