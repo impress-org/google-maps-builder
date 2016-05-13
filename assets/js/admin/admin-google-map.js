77ce4a770a35e98f5d1812ec9fded4cf01ecedd5
Devin Walker <devin@wordimpress.com>
2016-05-13 16:32:45 -0700

    Improved description for geolocation feature Default setting is now turned off...

--- assets/js/admin/admin-google-map.js
+++ assets/js/admin/admin-google-map.js
@@ -315,7 +315,7 @@ var gmb_data;
 		places_service = new google.maps.places.PlacesService( map );
 
 		//Handle Map Geolocation
-		if ( navigator.geolocation && gmb_data.geolocate_setting === 'yes' && longitude == '' && latitude == '' ) {
+		if ( navigator.geolocation && gmb_data.geolocate_setting === 'yes' && longitude == '' && latitude == '' && location.protocol === 'https:' ) {
 			navigator.geolocation.getCurrentPosition( function ( position ) {
 				initial_location = new google.maps.LatLng( position.coords.latitude, position.coords.longitude );
 				map.setCenter( initial_location ); //set map with location
