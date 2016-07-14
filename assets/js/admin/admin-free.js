67b5f3db5b2effb857c4363342ed27e26f150c52
Devin Walker <devin@wordimpress.com>
2016-07-14 16:51:49 -0700

    Removing scripts not necessary because they are in core

--- assets/js/admin/admin-free.js
+++ assets/js/admin/admin-free.js
@@ -1,88 +1,10 @@
-
+/**
+ * Admin Free Google Maps
+ *
+ * @description: Enqueued on the single `google_maps` CPT and responsible for interface map creation
+ */
 (function ( $, gmb ) {
 
-    /**
-     *  Add Markers
-     *
-     * This is the marker that first displays on load for the main location or place
-     *
-     * @param map
-     */
-    gmb.add_markers = function ( map ) {
-
-        gmb.clear_main_markers();
-
-        //Loop through repeatable field of markers
-        $( "#gmb_markers_group_repeat" ).find( '.cmb-repeatable-grouping' ).each( function ( index ) {
-
-            var marker_icon = gmb_data.default_marker;
-            var marker_label = '';
-
-            //check for custom marker and label data
-            var custom_marker_icon = $( '#gmb_markers_group_' + index + '_marker' ).val();
-            var custom_marker_img = $( '#gmb_markers_group_' + index + '_marker_img' ).val();
-
-            //check for custom marker and label data
-            if ( custom_marker_img ) {
-                marker_icon = custom_marker_img;
-            } else if ( custom_marker_icon.length > 0 && custom_marker_icon.length > 0 ) {
-                var custom_label = $( '#gmb_markers_group_' + index + '_label' ).val();
-                marker_icon = eval( "(" + custom_marker_icon + ")" );
-                marker_label = custom_label;
-            }
-
-
-            //Marker for map
-            var location_marker = new Marker( {
-                map: map,
-                zIndex: 9,
-                icon: marker_icon,
-                custom_label: marker_label
-            } );
-
-            var marker_lat = $( '#gmb_markers_group_' + index + '_lat' ).val();
-            var marker_lng = $( '#gmb_markers_group_' + index + '_lng' ).val();
-
-            location_marker.setPosition( new google.maps.LatLng( marker_lat, marker_lng ) );
-            location_marker.setVisible( true );
-
-            google.maps.event.addListener( location_marker, 'click', function () {
-                gmb.get_info_window_content( index, location_marker );
-            } );
-
-            //Remove row button/icon also removes icon (CMB2 buttons)
-            $( '#gmb_markers_group_' + index + '_title' ).parents( '.cmb-repeatable-grouping' ).find( '.cmb-remove-group-row' ).each( function () {
-                google.maps.event.addDomListener( $( this )[ 0 ], 'click', function () {
-                    var index = $( this ).parents( '.cmb-repeatable-grouping' ).data( 'index' );
-                    //close info window and remove marker
-                    info_bubble.close();
-                    location_marker.setVisible( false );
-                } );
-            } );
-
-        } ); //end $.each()
-
-    };
-
-    gmb.get_marker_index = function(){
-        //Create a new marker repeatable meta group
-        var index = parseInt( $( '#gmb_markers_group_repeat div.cmb-repeatable-grouping' ).last().attr( 'data-iterator' ) );
-        var existing_vals = $( 'div[data-iterator="0"] ' ).find( 'input,textarea' ).val();
-
-        //Ensure appropriate index is used for marker
-        if ( existing_vals && index === 0 ) {
-            $( '.cmb-add-group-row.button' ).trigger( 'click' );
-            index = 1;
-        } else if ( index !== 0 ) {
-            $( '.cmb-add-group-row.button' ).trigger( 'click' );
-            //recount rows
-            index = parseInt( $( '#gmb_markers_group_repeat div.cmb-repeatable-grouping' ).last().attr( 'data-iterator' ) );
-        }
-        return index;
-    }
 
 
 }( jQuery, window.MapsBuilderAdmin || ( window.MapsBuilderAdmin = {} ) ) );
-
-
-
--- includes/class-gmb-scripts.php
+++ includes/class-gmb-scripts.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Scripts
+ * Maps Builder Free Version Scripts
  *
  * @package     GMB
  * @subpackage  Functions
  * @copyright   Copyright (c) 2016, WordImpress
  * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
- * @since       2.0
+ * @since       2.1
  */
 
 // Exit if accessed directly
@@ -22,7 +22,7 @@ class Google_Maps_Builder_Scripts extends Google_Maps_Builder_Core_Scripts_Init 
 	/**
 	 * Load additional admin scripts
 	 *
-	 * @since 2.1.0
+	 * @since 2.1
 	 *
 	 * @uses "admin_enqueue_scripts"
 	 *
@@ -34,16 +34,8 @@ class Google_Maps_Builder_Scripts extends Google_Maps_Builder_Core_Scripts_Init 
 		$suffix = $this->paths->suffix();
 
 		if ( ( $hook == 'post-new.php' || $hook == 'post.php' ) && 'google_maps' === $post->post_type ) {
-			//pro only
-			wp_register_script( $this->plugin_slug . '-admin-free', $js_dir . 'admin-free' . $suffix . '.js', array( 'jquery' ), GMB_VERSION );
-			wp_enqueue_script( $this->plugin_slug . '-admin-free' );
 
-		}
-
-		//Import/Export Scripts
-		if ( $hook == 'google_maps_page_gmb_import_export' ) {
-			wp_register_script( $this->plugin_slug . '-admin-import-export', $js_dir . 'admin-import-export' . $suffix . '.js', array( 'jquery' ), GMB_VERSION );
-			wp_enqueue_script( $this->plugin_slug . '-admin-import-export' );
+			//free only
 
 
 		}
